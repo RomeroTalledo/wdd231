157a425fbf32e335637aa75d5faf2325e5e65c0d
Dallin Romero <dallinromero2002@gmail.com>
2026-01-30 00:15:45 -0500

    week03

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,16 +2,14 @@ const weatherData = {
   current: {
     temp: 75,
     desc: "Partly Cloudy",
-    high: 85,
-    low: 52,
     humidity: 34,
     sunrise: "7:30 AM",
     sunset: "5:59 PM"
   },
   forecast: {
-    today: "Today: 90°F",
-    tomorrow: "Wednesday: 89°F",
-    day2: "Thursday: 68°F"
+    today: "Today: 75°F",
+    tomorrow: "Tomorrow: 78°F",
+    day2: "Next Day: 72°F"
   }
 };
 
@@ -22,8 +20,6 @@ function setText(id, text) {
 
 setText("temp", `Temp: ${weatherData.current.temp}°F`);
 setText("desc", `Sky: ${weatherData.current.desc}`);
-setText("high", `High: ${weatherData.current.high}°F`);
-setText("low", `Low: ${weatherData.current.low}°F`);
 setText("humidity", `Humidity: ${weatherData.current.humidity}%`);
 setText("sunrise", `Sunrise: ${weatherData.current.sunrise}`);
 setText("sunset", `Sunset: ${weatherData.current.sunset}`);
